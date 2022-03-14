ecf12b2d51269a39c5a6c4d441c6bb28310aa73e
Wilson Oen <klo5@sfu.ca>
2022-03-13 19:47:25 -0700

    fixed incorrect user number

--- tools/gatling-50-user.sh
+++ tools/gatling-50-user.sh
@@ -4,7 +4,7 @@ docker container run --detach --rm \
   -v ${PWD}/gatling:/opt/gatling/user-files \
   -v ${PWD}/gatling/target:/opt/gatling/target \
   -e CLUSTER_IP=`tools/getip.sh kubectl istio-system svc/istio-ingressgateway` \
-  -e USERS=10 \
+  -e USERS=50 \
   -e SIM_NAME=ReadUserSim \
   --label gatling \
   ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
