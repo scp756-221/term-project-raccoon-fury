f4dddac60961254eee65297739efc51e2292a749
Wilson Oen <klo5@sfu.ca>
2022-04-03 00:05:40 -0700

    Set up gatling scripts for user and music

---
+++ tools/gatling-n-music.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Enter number of user for your load test."
+	echo "Example: tools/gatling-n-music.sh 200"
+fi
+
+docker container run --detach --rm \
+  -v ${PWD}/gatling/results:/opt/gatling/results \
+  -v ${PWD}/gatling:/opt/gatling/user-files \
+  -v ${PWD}/gatling/target:/opt/gatling/target \
+  -e CLUSTER_IP=`tools/getip.sh kubectl istio-system svc/istio-ingressgateway` \
+  -e USERS=$1 \
+  -e SIM_NAME=ReadMusicSim \
+  --label gatling \
+  ghcr.io/scp-2021-jan-cmpt-756/gatling:3.4.2 \
+  -s proj756.ReadMusicSim
